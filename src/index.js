7e253c7b53d4aa47eb634e05206b464eb1e877be
Erik <eguntner2291@gmail.com>
2018-07-06 17:35:52 -0700

    finished final boiler plater

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App.jsx';
 
 const root = document.getElementById('root');
 
-render(<div>jsx tagera</div>, root);
+render(<App />, root);

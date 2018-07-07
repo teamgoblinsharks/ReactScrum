3d687db8fbe4c1e4b0091c9df84aa70673149dfa
pvalderama <34151463+pvalderama@users.noreply.github.com>
2018-07-06 17:13:37 -0700

    paul

---
+++ temporary/AddTask.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class AddTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  };
+  render(){
+    return (
+      <div>
+      <button>Add Task</button>
+      </div>
+    )
+  }
+} 
+
+export default AddTask; 

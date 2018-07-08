45dec8e14c66840ec3151351b432cc34a7837ac3
Brendan Morrell <brendanmorrell@gmail.com>
2018-07-08 14:29:25 -0700

    changed dummy data by removing unnecessary fields

--- src/reducers/boardsReducer.js
+++ src/reducers/boardsReducer.js
@@ -4,14 +4,10 @@ const initialState = [
   {
     _id: 1,
     name: 'final project',
-    stories: [1, 1.1, 1.2],
-    tasks: [1.0, 1.01, 1.02, 1.1, 1.11, 1.12, 1.2, 1.21, 1.22],
   },
   {
     _id: 2,
     name: 'work plan',
-    stories: [2, 2.1, 2.2],
-    tasks: [2.0, 2.01, 2.02, 2.1, 2.11, 2.12, 2.2, 2.21, 2.22],
   },
 ];
 
